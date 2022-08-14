00c0efc15fbf633414b67b3f22753d63bb23c66b
Uvais Ahmad <uvss21@gmail.com>
2022-08-14 20:55:49 +0530

    Make cartItem class compo to func compo

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,8 @@
 //in react we use CamlinCase of naming convention
 import React from "react";
 
-class CartItem extends React.Component{
+//This conver from class component to Functional component so bydefault props argu used
+const CartItem = (props) =>{
     
     increaseQuantity = () => {
         // This is pbject element to set value use .setState() function gives value in object
@@ -54,62 +55,57 @@ class CartItem extends React.Component{
         }
     }
     
-    render (){
-        console.log(this.props);
-        // Used for short initialize object fromthe constructor
-        
-        const { title , price , qty , img} = this.props.product;
-        // const {
-        //     product,
-        //     onIncreaseQuantity,
-        //     onDecreasingQuantity,
-        //     onDeleting
-        // } = this.props;
-        return (
-            <div className="cart-Item">                             
-                <div className="left-block">
-                    {/* Here we add the style CSS */}
-                    {/* src={this.state.img} */}
-                    <img style={style.Image} src={img}  alt=""/>
-                </div>
+    console.log(this.props);
+    // Used for short initialize object fromthe constructor
+    
+    const { title , price , qty , img} = this.props.product;
+    // const {
+    //     product,
+    //     onIncreaseQuantity,
+    //     onDecreasingQuantity,
+    //     onDeleting
+    // } = this.props;
+    return (
+        <div className="cart-Item">                             
+            <div className="left-block">
+                {/* Here we add the style CSS */}
+                {/* src={this.state.img} */}
+                <img style={style.Image} src={img}  alt=""/>
+            </div>
 
-                <div className="right-block">
-                    <div style={{fontSize:25}}>{title}</div>
-                    <div style={{color:'lightblue'}}>{price}</div>
-                    <div>{qty}</div>
-                    <div className="cart-item-actions">
-                        {/* This is three icon will be added in 'img' Tag */}
+            <div className="right-block">
+                <div style={{fontSize:25}}>{title}</div>
+                <div style={{color:'lightblue'}}>{price}</div>
+                <div>{qty}</div>
+                <div className="cart-item-actions">
+                    {/* This is three icon will be added in 'img' Tag */}
 
-                        <img 
-                        alt="increase" 
-                        src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" 
-                        className="action-icon"
-                        // onClick= {this.increaseQuantity}
-                        // Now we have function in props used to increase and sent from CART
-                        onClick = {()=>this.props.onIncreaseQuantity(this.props.product)}
-                        />
-
-                        <img 
-                        alt="decrease" 
-                        src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" 
-                        className="action-icon" 
-                        // onClick={this.decreaseQuantity}
-                        //Each CartItem Hold the property trto decre in props argu
-                        onClick={()=>this.props.onDecreasingQuantity(this.props.product)}
-                        />
-
-                        <img 
-                        alt="delete" 
-                        src="https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg" 
-                        className="action-icon"
-                        onClick={()=> this.props.onDeleting(this.props.product.id)}
-                        />  
-
-                    </div>
+                    <img 
+                    alt="increase" 
+                    src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" 
+                    className="action-icon"
+                    // onClick= {this.increaseQuantity}
+                    // Now we have function in props used to increase and sent from CART
+                    onClick = {()=>this.props.onIncreaseQuantity(this.props.product)}
+                    />
+                    <img 
+                    alt="decrease" 
+                    src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" 
+                    className="action-icon" 
+                    // onClick={this.decreaseQuantity}
+                    //Each CartItem Hold the property trto decre in props argu
+                    onClick={()=>this.props.onDecreasingQuantity(this.props.product)}
+                    />
+                    <img 
+                    alt="delete" 
+                    src="https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg" 
+                    className="action-icon"
+                    onClick={()=> this.props.onDeleting(this.props.product.id)}
+                    />  
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 //Here we set style in Object and byDefault is in pixels
